01f56bd99a2192b77fa88b43ad0421a9528e1f2d
Martin Werner <martin.werner@tum.de>
2023-07-13 09:39:56 +0200

    extract generic API into one function

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ void add_generic_api_functions(classtype cls)
 template<typename pointcloud_type, typename module_type>
 void register_class_with_name3(module_type m, std::string name)
 {
- pybind11::class_<pointcloud_type>(m, name.c_str())
+    auto cls = pybind11::class_<pointcloud_type>(m, name.c_str())
     .def(pybind11::init())
     .def("add", [](pointcloud_type &self, py::array_t<double, py::array::c_style | py::array::forcecast> pcl)
     {
@@ -72,40 +72,12 @@ void register_class_with_name3(module_type m, std::string name)
 	  if (r.shape(1) != 3) throw(std::runtime_error("Expect Nx3 matrix"));
 	  self.cloud.push_back(typename pointcloud_type::point_type(r(i, 0),r(i,  1),r(i,  2)));
 	}
-    })
-    .def("size",[](const pointcloud_type &self) {
-      return self.cloud.size();
-    })
-    .def("attributes",[](const pointcloud_type &self){
-        std::string attributes;
-	for (const auto & s: self.named_attributes)
-	   attributes +"=" + s.first;
-	return attributes;
-    })
-    .def("index",[](pointcloud_type &self){
-      self.buildIndex();
-    })
-    .def("knn_radius",[](pointcloud_type &self, std::string name, int k){
-        auto ret = self.computeKNNradius(name,k);
-    })
-    .def ("get_attrib",[](pointcloud_type &self, std::string name){
-       auto result = self.get_attrib_by_name(name);
-       return py::array_t<double>(result.size(), result.data()); 
+    });
+    add_generic_api_functions<decltype(cls),pointcloud_type>(cls);
 
-    })
-    .def("eigenfeatures_knn",[](pointcloud_type &self, std::string prefix, int k){
-	self.map_kNN_features(k, prefix);
-    })
-    .def("eigenfeatures_range",[](pointcloud_type &self, std::string prefix, double r){
-	self.map_3drange_features(r, prefix);
-    })
-
-
-    ; // keep on line to make new methods easier to add.
- 
 
 };
-// todo: update registerclasswithname3 to use generic api
+
 template<typename pointcloud_type, typename module_type>
 void register_class_with_name4(module_type m, std::string name)
 {
